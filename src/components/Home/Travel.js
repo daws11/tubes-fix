4bd82a3ddc7b268c74d46ebf5780c8b48f1af423
daws11 <dawskutel@gmail.com>
2022-12-26 13:44:16 +0700

    fix presentasi

---
+++ src/components/Home/Travel.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Travel = () => {
+  return (
+    
+         <div className="travel-section">
+          <h1>Travel Anywhere</h1>
+          <div className="travel-box">
+            <section>
+              <img src="img/travel/t1.jpg" alt="" />
+              <p>Bandung</p>
+            </section>
+            <section>
+              <img src="img/travel/t3.jpg" alt="" />
+              <p>Jakarta</p>
+            </section>
+            <section>
+              <img src="img/travel/t4.jpg" alt="" />
+              <p>Yogyakarta</p>
+            </section>
+            <section>
+              <img src="img/travel/t2.jpg" alt="" />
+              <p> Cirebon</p>
+            </section>
+            <section>
+              <img src="img/travel/t6.jpg" alt="" />
+              <p>Semarang</p>
+            </section>
+            <section>
+              <img src="img/travel/t7.jpg" alt="" />
+              <p>Bali</p>
+            </section>
+          </div>
+        </div>
+    
+  )
+}
+
+export default Travel
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+//importing error boundary
+import ErrorBoundary from "./components/ErrorBoundary";
+
+// importing Provider Wrapper
+import { Provider } from "react-redux";
+
+//importing redux store
+import store from "./redux/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
 
